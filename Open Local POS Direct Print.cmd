229cd7610529dc989285ef22a2ff410b5221f642
Afsal Rahman <afsal71@gmail.com>
2026-09-26 01:31:37 +0530

    final  commit

--- Open Local POS Direct Print.cmd
+++ Open Local POS Direct Print.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\start-direct-print.ps1" -Url "http://localhost:3000"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\start-direct-print.ps1" -Url "https://oushadikkmpos.vercel.app"
 if errorlevel 1 pause
